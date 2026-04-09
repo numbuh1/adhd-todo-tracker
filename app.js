ff55d6c813796dd1cd163121f1214b9cf9ec56a2
HuyNLQ <huy.nguyen@ascension6.com.au>
2026-04-09 21:58:59 +0700

    hotfix link

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const viewRouter = require('./routes/viewRouter');
 const adminRouter = require('./routes/adminRouter');
-const mainRouter = express.Router();
-
-const BASE_PATH = process.env.BASE_PATH || '';
 
 // Connect to MongoDB
 connectDB();
@@ -24,19 +21,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(BASE_PATH, express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Make __dirname available in all views (for include() absolute paths)
 app.locals.root = __dirname;
 
 // ── Routes ─────────────────────────────────────────────────
-// mount your routers INSIDE
-mainRouter.use('/', viewRouter);
-mainRouter.use('/admin', adminRouter);
-
-// then mount once
-app.use(BASE_PATH, mainRouter);
+app.use('/', viewRouter);
+app.use('/admin', adminRouter);
 
 // ── Error Handler ──────────────────────────────────────────
 app.use((err, req, res, next) => {
